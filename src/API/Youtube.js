53e41e1cb53a5c840897c845a457005319864a8c
Sanju <imatom19@gmail.com>
2019-06-29 01:32:53 +0530

    no rstrict api

--- src/API/Youtube.js
+++ src/API/Youtube.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyCESu55UdSNFo3rlGu7V2iWpwZTKb6DtZU';
-// 'AIzaSyDHrEJ4SLaQdqmY84jwoX1hPgrr54G-mN4';
+const KEY = 'AIzaSyB0NeKjCw7imbEVcBXGipd_g1n4PU6XRqc';
+// 'AIzaSyCESu55UdSNFo3rlGu7V2iWpwZTKb6DtZU'; imatom
+// 'AIzaSyDHrEJ4SLaQdqmY84jwoX1hPgrr54G-mN4'; sanjoy.k
 
 export default axios.create({
     baseURL:'https://www.googleapis.com/youtube/v3',
